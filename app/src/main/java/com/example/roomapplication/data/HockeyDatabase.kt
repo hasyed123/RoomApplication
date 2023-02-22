02156d319a7b845840643e3e27c7e864cc612a4b
Hasan Syed <hasansyed12s@outlook.com>
2023-02-22 00:13:07 -0500

    fixed import issue

--- app/src/main/java/com/example/roomapplication/data/HockeyDatabase.kt
+++ app/src/main/java/com/example/roomapplication/data/HockeyDatabase.kt
@@ -2,6 +2,10 @@ package com.example.roomapplication.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.roomapplication.data.league.League
+import com.example.roomapplication.data.league.LeagueDao
+import com.example.roomapplication.data.team.Team
+import com.example.roomapplication.data.team.TeamDao
 
 @Database(entities = [League::class, Team::class], version = 1, exportSchema = false)
 abstract class HockeyDatabase: RoomDatabase() {
